a402a9a8aa8633cbe15e4fd82ed73b17b9548d71
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2024-06-03 19:19:48 -0300

    BlogAPI

--- EJS/EJSTags/index.js
+++ EJS/EJSTags/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
 
 app.get("/", (req, res) => {
   const data = {
